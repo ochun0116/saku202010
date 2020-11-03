1ea95f9f2ed2b03b81ce03ef7d1696bf12bfc851
yoshi <yoshitakaOkada0214@gmail.com>
2020-11-03 12:32:33 +0900

    feature search

--- shop/models.py
+++ shop/models.py
@@ -2,16 +2,30 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+
+class Category(models.Model):
+    """商品カテゴリー"""
+    name = models.CharField(verbose_name='カテゴリ', max_length=100, blank=False, null=False)
+
+    def __str__(self):
+        if hasattr(self, 'product_count'):
+            return f'{self.name}({self.product_count})'
+        return self.name
+
+
 class Product(models.Model):
-    '''商品'''
+    """商品"""
     name = models.CharField(verbose_name='名前', max_length=200, blank=False, null=False)
-    category = models.CharField(verbose_name='カテゴリ', max_length=100, blank=False, null=False)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    price = models.IntegerField(verbose_name='単価',)
+    description = models.CharField(verbose_name='説明', max_length=100, blank=False, null=False)
     inactive = models.BooleanField(default=False)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField('作成日時', auto_now_add=True)
 
+
 class Discussion(models.Model):
-    '''会話'''
+    """会話"""
     chat = models.TextField(verbose_name='会話')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     created_at = models.DateTimeField('作成日時', auto_now_add=True)
--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,38 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.urls import reverse_lazy, reverse
 from django.views import generic
 from django.views.generic import CreateView
 
-from .forms import DiscussionForm, ProductRegisterForm
-from .models import Product, Discussion
+from .forms import DiscussionForm, ProductRegisterForm, SearchForm
+from .models import Product, Discussion, Category
 
 
 class ProductListView(generic.ListView):
     """ / で product 一覧."""
     model = Product
+    queryset = Product.objects.filter(inactive=False)
+    ordering = '-created_at'
+
+    def get_queryset(self):
+        """検索された結果を返します"""
+        queryset = super().get_queryset()
+        self.form = SearchForm(self.request.GET or None)
+        if self.form.is_valid():
+            category = self.form.cleaned_data.get('category')
+            if category:
+                queryset = queryset.filter(category=category)
+
+            keyword = self.form.cleaned_data['keyword']
+            if keyword:
+                queryset = queryset.filter(Q(name__icontains=keyword)).distinct()
+
+        return queryset.select_related('category')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_form'] = self.form
+        return context
 
 
 class ProductDetailView(generic.DetailView):
@@ -52,4 +75,5 @@ class ProductRegister(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.user_id = self.request.user.id
+        form.instance.category_id = Category.objects.filter(name=form.cleaned_data["category"]).values("id")
         return super().form_valid(form)
